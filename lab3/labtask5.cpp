6bfe4f2a46e8cc5bfe42e35e7da6f4d537475dd1
ateeb <132497339+ate-eb@users.noreply.github.com>
2025-10-04 22:55:53 +0500

    Create labtask5.cpp

---
+++ lab3/labtask5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Concrete{
+	private:
+		string message;
+		public:
+			Concrete(string msg){
+				message = msg;
+			}
+			void displayMessage(){
+				cout<<message<<endl;
+			}
+};
+int main(){
+	Concrete c("I hate playing clash royale ");
+	c.displayMessage();
+}
